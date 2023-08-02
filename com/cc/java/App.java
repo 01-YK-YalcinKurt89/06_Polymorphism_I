d08d821c5d2ece0c30739a271683f3cd15696708
Yalcin <yalcin.coding@gmail.com>
2023-08-02 10:07:26 +0200

    added some files

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,24 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    Drone drone = new Drone();
+    Worker worker = new Worker();
+    Queen queen = new Queen();
         
+
+    output(queen.doYourJob());
+    output(worker.doYourJob());
+    output(drone.doYourJob());
+    
+    output("---------------------");
+
+    output(queen.fly());
+    output(worker.fly());
+    output(drone.fly());
+
+
+
+
     }
 
    
