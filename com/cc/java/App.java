d09ee75a59a332c2c005ccf881c92d75430d12c1
Yalcin <yalcin.coding@gmail.com>
2023-08-02 10:47:16 +0200

    added pollObj()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,30 +1,38 @@
 package com.cc.java;
 
 public class App {
+    // _____ 2. Iteration :: Overloading _____ //
     
     public static void main(String[] args) {
     Drone drone = new Drone();
     Worker worker = new Worker();
     Queen queen = new Queen();
         
-
-    output(queen.doYourJob());
-    output(worker.doYourJob());
-    output(drone.doYourJob());
-    
-    output("---------------------");
-
-    output(queen.fly());
-    output(worker.fly());
-    output(drone.fly());
-
-
-
-
+    pollObj(queen);
+    pollObj(worker);
+    pollObj(drone);
     }
 
-   
-   
+
+    private static void pollObj(Queen obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("---------------------");
+    }
+
+    private static void pollObj(Worker obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("---------------------");
+    }
+
+    private static void pollObj(Drone obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("---------------------");
+    }
+
+    
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
--- com/cc/java/Drone.java
+++ com/cc/java/Drone.java
@@ -1,7 +1,7 @@
 package com.cc.java;
 
 public class Drone extends HoneyBee{
-    // public String doYourJob() {
-    //     return "I'm a drone, I'm going to date our Queen!";
-    // }
+    public String doYourJob() {
+        return "I'm a drone, I'm going to date our Queen!";
+    }
 }
